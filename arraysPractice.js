571f58e9923d566ab0763c9c28abb23af3b9a704
SJTreadway <treadway08@gmail.com>
2015-10-26 15:56:39 -0500

    completes more problems

--- arraysPractice.js
+++ arraysPractice.js
@@ -15,12 +15,12 @@ function first(arr) {
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
+
+
+  //Code Here
 function last(arr) {
   return arr.pop();
 }
-
-  //Code Here
-
 
 //Next Problem
 
@@ -29,7 +29,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
-
+  function looper(family) {
+    for (var i = 0; i < family.length; i++) {
+      alert(family[i]);
+    }
+  }
 
 //Next problem
 
@@ -39,7 +43,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
-
+function reverseLooper(letters) {
+  for (var i = letters.length-1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 //Next Problem
 
